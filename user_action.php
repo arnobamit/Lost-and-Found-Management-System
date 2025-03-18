41f8f1129da7035a0f4455a46961e4e79dff332c
miskatjahan01 <miskatjahan988@gmail.com>
2025-03-18 16:05:31 +0600

    Add files via upload

---
+++ user_action.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+         
+        <link rel="stylesheet" type="text/css" href="CSS/user reg.css">
+    </head>
+    <body>
+         <h3>This registration is successful!</h3>
+    </body>
+</html>
